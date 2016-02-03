4bd37a0375d665d5238bb1f20c28fede131f92cd
Дмитрий Фалько < >
2016-02-03 22:33:01 +0300

    Initial

---
+++ main.c
@@ -0,0 +1,53 @@
+//***************************************************************************************
+//  MSP430 Blink the LED Demo - Software Toggle P1.0
+//
+//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//                MSP430x5xx
+//             -----------------
+//         /|\|              XIN|-
+//          | |                 |
+//          --|RST          XOUT|-
+//            |                 |
+//            |             P1.0|-->LED
+//
+//  J. Stevenson
+//  Texas Instruments, Inc
+//  July 2011
+//  Built with Code Composer Studio v5
+//***************************************************************************************
+
+#include <msp430.h>				
+
+int main(void)
+{
+  WDTCTL = WDT_ADLY_16;
+  IE1 |= WDTIE;
+  P1DIR |= 0x0D;                            // P1.2 and P1.3 output
+  P1SEL |= 0x0C;                            // P1.2 and P1.3 TA1/2 options
+  CCR0 = 512-1;                             // PWM Period
+  CCTL1 = OUTMOD_7;                         // CCR1 reset/set
+  CCR1 = 400;                               // CCR1 PWM duty cycle
+  TACTL = TASSEL_2 + MC_1;                  // SMCLK, up mode
+
+  __bis_SR_register(CPUOFF + GIE);          // Enter LPM0 w/interrupt
+}
+
+// Watchdog Timer interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=WDT_VECTOR
+__interrupt void watchdog_timer(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(WDT_VECTOR))) watchdog_timer (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  P1OUT ^= 0x01;
+  if(CCR1 <= 100) {
+	  CCR1 = 500;
+  } else {
+	  CCR1 -= 20;
+  }
+}
