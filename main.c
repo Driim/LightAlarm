8186d2d959c881c9a6bee938fc236270bba5572e
Dmitry Falko <Dmitry Falko>
2016-02-04 14:49:23 +0300

    Additional info

--- main.c
+++ main.c
@@ -1,48 +1,51 @@
 //***************************************************************************************
-//  MSP430 Blink the LED Demo - Software Toggle P1.0
+//  MSP430 Light Alarm
 //
-//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
-//  ACLK = n/a, MCLK = SMCLK = default DCO
+//  Description: Световой будильник управляемый по bluetooth. 
+//               Используем Timer0_A3 и Timer1_A3 в режиме ШИМ 
+//               для управления тремя светодиодами.
 //
-//                MSP430x5xx
+//                MSP4302553
 //             -----------------
 //         /|\|              XIN|-
 //          | |                 |
 //          --|RST          XOUT|-
 //            |                 |
-//            |             P1.0|-->LED
+//            |             P1.6|-->PWM LED
+//            |                 |
+//            |             P2.1|-->PWM LED
+//            |                 |
+//            |             P2.4|-->PWM LED
 //
-//  J. Stevenson
-//  Texas Instruments, Inc
-//  July 2011
-//  Built with Code Composer Studio v5
+//  D. Falko
+//  February 2016
+//  Built with Code Composer Studio v6
 //***************************************************************************************
 
 #include <msp430.h>				
 
 int main(void)
 {
-  WDTCTL = WDT_MDLY_32;
-  IE1 |= WDTIE;
+  WDTCTL = WDT_MDLY_32;                        // Watchdog clock source
+  IE1 |= WDTIE;                                // Watchdog Interrupt enable
 
-
-  P1DIR |= BIT6;                            // P1.2 and P1.3 output
-  P1SEL |= BIT6;                            // P1.2 and P1.3 TA1/2 options
+  P1DIR |= BIT6;                               // P1.6 
+  P1SEL |= BIT6;                               // P1.6 TA1/2 options
   TA0CCR0 = 512-1;                             // PWM Period
   TA0CCTL1 = OUTMOD_7;                         // CCR1 reset/set
   TA0CCR1 = 400;                               // CCR1 PWM duty cycle
-  TA0CTL = TASSEL_2 + MC_1;                  // SMCLK, up mode
+  TA0CTL = TASSEL_2 + MC_1;                    // SMCLK, up mode
 
-  P2DIR |= BIT1 | BIT4;                            // P1.2 and P1.3 output
-  P2SEL |= BIT1 | BIT4;                            // P1.2 and P1.3 TA1/2 options
+  P2DIR |= BIT1 | BIT4;                        // P2.1 and P2.4 output
+  P2SEL |= BIT1 | BIT4;                        // P2.1 and P2.4 TA1/2 options
   TA1CCR0 = 512-1;                             // PWM Period
   TA1CCTL1 = OUTMOD_7;                         // CCR1 reset/set
   TA1CCR1 = 400;                               // CCR1 PWM duty cycle
   TA1CCTL2 = OUTMOD_7;                         // CCR1 reset/set
   TA1CCR2 = 400;                               // CCR1 PWM duty cycle
-  TA1CTL = TASSEL_2 + MC_1;                  // SMCLK, up mode
+  TA1CTL = TASSEL_2 + MC_1;                    // SMCLK, up mode
 
-  __bis_SR_register(CPUOFF + GIE);          // Enter LPM0 w/interrupt
+  __bis_SR_register(CPUOFF + GIE);             // Enter LPM0 w/interrupt
 }
 
 // Watchdog Timer interrupt service routine
@@ -55,6 +58,7 @@ void __attribute__ ((interrupt(WDT_VECTOR))) watchdog_timer (void)
 #error Compiler not supported!
 #endif
 {
+  // 
   if(TA0CCR1 <= 100) {
 	  TA0CCR1 = 500;
   } else {
